f8278491d92310e99be9e9db9f9185b7405eb119
Simon Busch <simon@busch.pro>
2022-10-20 04:56:59 +0200

    Added React components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Links } from './pages/links/Links';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/links">Links</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/links" element={<Links />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/links/Links.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Links() {
+  return (
+    <div>
+      <h2>Links</h2>
+    </div>
+  );
+}
